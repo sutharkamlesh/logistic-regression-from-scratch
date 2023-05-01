79e06c0457913e9f662dd468782d13f9bb252f14
“Kamlesh <kamlesh08@live.com>
2023-05-01 23:11:11 +0530

    Bias term is updated seperately in each iteration of Gradient Decent

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -14,22 +14,23 @@ def train_test_split(X, y, test_size=0.3):
     return X[indexes[sample_size:]], X[indexes[:sample_size]], y[indexes[sample_size:]], y[indexes[:sample_size]]
 
 
-class LogisticRegression():
+class CustomLogisticRegression():
     # TODO: Make this class work for multi-class problem
-    def __init__(self, C = 0.5, learning_rate=0.01, initial_weights=None, max_iter=100, penalty=None, sample_size=1):
+    def __init__(self, C = 0.5, learning_rate=0.01, max_iter=100, penalty=None, sample_size=1):
         self.C = C
         self.max_iter = max_iter
         self.penalty = penalty
         self.learning_rate = learning_rate
         self.sample_size = sample_size
-        self.weights = initial_weights
+        self.weights = None
+        self.bias = None
         self.history = {
             "costs": [],
             "accuracy": []
         }
 
     def predict_proba(self, X):
-        return sigmoid(np.dot(X, self.weights))
+        return sigmoid(np.dot(X, self.weights) + self.bias)
 
     def predict(self, X, threshold=0.5):
         # TODO: Tune threshold to get maximum accuracy
@@ -50,9 +51,10 @@ class LogisticRegression():
         return X[sample_idx], y[sample_idx]
 
     def update_weights(self, X, y):
-        self.weights = self.weights - self.learning_rate * (np.dot(X.T, self.predict_proba(X) - y) + self.penalty_term) / \
-                        X.shape[0]
-        print(self.weights)
+        self.weights = self.weights - self.learning_rate * (np.dot(X.T, self.predict_proba(X) - y)) / X.shape[0]
+        
+    def update_bias(self, X, y):
+        self.bias = self.bias - (self.learning_rate * np.mean(self.predict_proba(X) - y))
 
     def cost(self, y_true, y_pred):
         cost = np.sum(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
@@ -64,9 +66,11 @@ class LogisticRegression():
     def fit(self, X, y):
         m, n = X.shape
         self.weights = np.random.randn(n)
+        self.bias = 0
         for _ in range(self.max_iter):
             sample_X, sample_y = self.sample(X, y)
             self.update_weights(sample_X, sample_y)
+            self.update_bias(sample_X, sample_y)
             self.history.get("costs").append(self.cost(y, self.predict_proba(X)))
             self.history.get("accuracy").append(self.score(y, self.predict(X)))
 
@@ -77,17 +81,32 @@ class LogisticRegression():
             return sum((y_true & y_predicted) | [ not i for i in (y_true | y_predicted)])/len(y_true)
 
 
-iris_data = datasets.load_iris()
+iris_data = datasets.load_breast_cancer()
 X = iris_data.get("data")
 m, n = X.shape
 X = (X - X.mean(axis=0))/X.std(axis=0)
-X = np.c_[np.ones(m), X]
+# X = np.c_[np.ones(m), X]
 y = (iris_data.get("target") == 0)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-clf = LogisticRegression(sample_size=64)
+clf = CustomLogisticRegression(sample_size=120, max_iter=1000)
 clf.fit(X_train, y_train)
-print("Weights: ", clf.weights)
+print("Weights: ", clf.weights, "\n", "Bias: ", clf.bias)
+
 preds = clf.predict(X_test)
 print("Accuracy: ", clf.score(y_test, preds))
+
+print("Event rate:", sum(y_test)/len(y_test))
+
+# Comparing the output of Sciket Learn Method 
+from sklearn.linear_model import LogisticRegression
+
+clf = LogisticRegression()
+clf.fit(X_train, y_train)
+print("Weights: ", clf.coef_, "\n", "Bias: ", clf.intercept_)
+
+preds = clf.predict(X_test)
+print("Accuracy: ", clf.score(y_test, preds))
+
+sum(y_test == preds)/len(y_test)
